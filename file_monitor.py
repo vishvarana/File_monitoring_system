a74e73ad6fe3042ec0d3585356b0f99a22927d61
vishvarana <85014366+vishvarana@users.noreply.github.com>
2022-01-28 18:46:40 +0530

    Add files via upload

--- file_monitor.py
+++ file_monitor.py
@@ -2,35 +2,35 @@ import asyncio
 import random
 import string
 
+
 class File_writer:
 
-    def __init__(self,file):
+    def __init__(self, file):
         self.file = file
 
     async def string_maker(self):
         n = 6
-        s1 = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k = n))
-        list = [s1,"MARUTI"]
+        s1 = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=n))
+        list = [s1, "MARUTI"]
         return random.choice(list)
 
-
     async def writer(self):
-        f = open(self.file,"a+")
+        f = open(self.file, "a+")
+        result = await self.string_maker()
+        f.write(result + " ")
+        f.close()
 
-        for i in range(100):
-            result = await self.string_maker()
-            f.write(result + " ")
-    
+
 class Counter:
 
-    def __init__(self,file1,file2):
+    def __init__(self, file1, file2):
         self.file1 = file1
         self.file2 = file2
 
-    def counter_class(self):
-        f1 = open(self.file1,"r")
-        f2 = open(self.file2,"r")
-        counter = open("count.log","w")
+    async def counter_class(self):
+        f1 = open(self.file1, "r")
+        f2 = open(self.file2, "r")
+        counter = open("count.log", "w")
 
         data1 = f1.read()
         data2 = f2.read()
@@ -40,14 +40,13 @@ class Counter:
         counter.write("Count for 'MARUTI' in {} is ".format(self.file1) + str(n1) + "\n")
         counter.write("Count for 'MARUTI' in {} is ".format(self.file2) + str(n2) + "\n")
 
-if __name__=="__main__":
 
-    while(True):
+async def main():
+    while True:
         writer1 = File_writer("file1.txt")
         writer2 = File_writer("file2.txt")
-        counter = Counter("file1.txt","file2.txt")
+        counter = Counter("file1.txt", "file2.txt")
+        await asyncio.gather(writer1.writer(), writer2.writer(), counter.counter_class())
 
-        asyncio.run(writer1.writer())
-        asyncio.run(writer2.writer())
 
-        counter.counter_class()
+asyncio.run(main())
